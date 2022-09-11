58e2d2e8603e5be09a078b24620e9cee0c3fe54b
otonielvieira <ottobuzios@gmail.com>
2022-09-11 07:17:53 -0300

    Criação do primeiro componente botao padrao. Configuração storybook terminada

---
+++ src/ui/components/imputs/RoundedButton/RoundedButton.stories.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import RoundedButton from "./RoundedButton";
+
+
+export default {
+    title: 'imputs/RoudedButton',
+    component: RoundedButton,
+} as ComponentMeta<typeof RoundedButton>;
+
+
+const Template: ComponentStory<typeof RoundedButton> = (args) => (
+    <RoundedButton {...args} >Click</RoundedButton>
+);
+
+
+export const Default = Template.bind({});
+
+Default.args = {
+    variant: "contained",
+};
+
+
+
+
+
+
+
