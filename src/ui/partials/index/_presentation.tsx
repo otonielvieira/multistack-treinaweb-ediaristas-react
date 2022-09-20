9f293e9c9138cfcba1d6e7df3790e38d669c5f30
otonielvieira <ottobuzios@gmail.com>
2022-09-20 07:22:02 -0300

    Estrutura base grid da tela de apresentacao

--- src/ui/partials/index/_presentation.tsx
+++ src/ui/partials/index/_presentation.tsx
@@ -1,8 +1,15 @@
-import { SectionContainer } from "./_presentation.stlyled";
+import { ContainerStyled, SectionContainer } from "./_presentation.stlyled";
 
 
 const Presentation = () => {
-    return <SectionContainer></SectionContainer>;
+    return <SectionContainer>
+        <ContainerStyled>
+            <div>titulo</div>
+            <div>subtitulo</div>
+            <div>button</div>
+            <div>imagem</div>
+        </ContainerStyled>
+    </SectionContainer>;
 }
 
 export default Presentation;
