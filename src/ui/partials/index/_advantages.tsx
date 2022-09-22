3f89d6f2baae61dd5b60181920008d667e6e9a98
otonielvieira <ottobuzios@gmail.com>
2022-09-22 19:57:05 -0300

    Criando componentes de vantagens avatar

--- src/ui/partials/index/_advantages.tsx
+++ src/ui/partials/index/_advantages.tsx
@@ -1,11 +1,82 @@
-import { Container } from "@mui/material";
-import { GradientBackground, SectionTitle } from "./_advantages.styled";
+import { Container, ListItem, ListItemAvatar } from "@mui/material";
+import { AvatarStyled, GradientBackground, ListItemTextStyled, ListStyle, SectionTitle } from "./_advantages.styled";
 
 const Advantages = () => {
     return (
         <GradientBackground>
             <Container>
                 <SectionTitle>Por que usar o E-diaristas?</SectionTitle>
+
+                <ListStyle>
+                    <ListItem>
+                        <ListItemAvatar>
+                            <AvatarStyled>
+                                <i className="twf-certificate" />
+                            </AvatarStyled>
+                        </ListItemAvatar>
+                        <ListItemTextStyled
+                            primary={'Diversidade'}
+                            secondary={"São mais de 5.000 profissionais esperando por você!"}
+                        />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemAvatar>
+                            <AvatarStyled>
+                                <i className="twf-certificate" />
+                            </AvatarStyled>
+                        </ListItemAvatar>
+                        <ListItemTextStyled
+                            primary={'Diversidade'}
+                            secondary={"São mais de 5.000 profissionais esperando por você!"}
+                        />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemAvatar>
+                            <AvatarStyled>
+                                <i className="twf-certificate" />
+                            </AvatarStyled>
+                        </ListItemAvatar>
+                        <ListItemTextStyled
+                            primary={'Diversidade'}
+                            secondary={"São mais de 5.000 profissionais esperando por você!"}
+                        />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemAvatar>
+                            <AvatarStyled>
+                                <i className="twf-certificate" />
+                            </AvatarStyled>
+                        </ListItemAvatar>
+                        <ListItemTextStyled
+                            primary={'Diversidade'}
+                            secondary={"São mais de 5.000 profissionais esperando por você!"}
+                        />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemAvatar>
+                            <AvatarStyled>
+                                <i className="twf-certificate" />
+                            </AvatarStyled>
+                        </ListItemAvatar>
+                        <ListItemTextStyled
+                            primary={'Diversidade'}
+                            secondary={"São mais de 5.000 profissionais esperando por você!"}
+                        />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemAvatar>
+                            <AvatarStyled>
+                                <i className="twf-certificate" />
+                            </AvatarStyled>
+                        </ListItemAvatar>
+                        <ListItemTextStyled
+                            primary={'Diversidade'}
+                            secondary={"São mais de 5.000 profissionais esperando por você!"}
+                        />
+                    </ListItem>
+
+                </ListStyle>
+
             </Container>
         </GradientBackground>
     );
