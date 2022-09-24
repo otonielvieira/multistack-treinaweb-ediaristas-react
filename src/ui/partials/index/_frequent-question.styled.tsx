28d3a610104084a9687d6c8b4b1f6b9b6b1b3fc0
otonielvieira <ottobuzios@gmail.com>
2022-09-24 13:44:37 -0300

    Criando componente de perguntas

--- src/ui/partials/index/_frequent-question.styled.tsx
+++ src/ui/partials/index/_frequent-question.styled.tsx
@@ -1,3 +1,4 @@
+import { Accordion } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
 
@@ -35,7 +36,38 @@ export const SectionSubtitle = styled('p')`
     height: 6px;
     left: 50%;
     transform: translate(-50%);
-    bottom: ${({theme})=>theme.spacing(-5)};
-    background-color: ${({theme})=>theme.palette.grey["300"]};
+    bottom: ${({ theme }) => theme.spacing(-5)};
+    background-color: ${({ theme }) => theme.palette.grey["300"]};
    }
+`;
+
+export const AccordionStyled = styled(Accordion)`
+   .MuiAccordionSummary-content .MuiTypography-root{
+        font-weight: bold;
+
+   } 
+
+   &.MuiAccordion-root{
+    box-shadow: none;
+    border: 2px solid ${({ theme }) => theme.palette.primary.main};
+
+    &::before{
+       background-color : transparent;
+       
+       &,
+       &.Mui-expanded{
+        margin: -2px 0 0;
+       }
+    }
+
+    .MuiAccordionDetails-root{
+       padding-right : ${({theme})=>theme.spacing(7)};
+    }
+
+    .MuiAccordionSummary-expandIconWrapper{
+       color: ${({theme})=>theme.palette.text.secondary}; 
+    }
+
+   }
+
 `;
--- src/ui/partials/index/_frequent-questions.tsx
+++ src/ui/partials/index/_frequent-questions.tsx
@@ -1,12 +1,29 @@
-import { SectionContainer, SectionSubtitle, SectionTitle, Wave } from "./_frequent-question.styled";
+import { AccordionDetails, AccordionSummary, Container, Typography } from "@mui/material";
+import { AccordionStyled, SectionContainer, SectionSubtitle, SectionTitle, Wave } from "./_frequent-question.styled";
 
 
 export const FrequentQuestion = () => {
     return (
         <SectionContainer>
             <Wave src={"/img/home/waves.svg"} />
-            <SectionTitle>Ainda está com dúvidas?</SectionTitle>
-            <SectionSubtitle>Veja abaixo as perguntas frequentes</SectionSubtitle>
+            <Container>
+                <SectionTitle>Ainda está com dúvidas?</SectionTitle>
+                <SectionSubtitle>Veja abaixo as perguntas frequentes</SectionSubtitle>
+
+                <AccordionStyled>
+
+                    <AccordionSummary expandIcon={<i className="twf-minus"/>}>
+                        <Typography color={"primary"}>Titulo</Typography>
+                    </AccordionSummary>
+
+                    <AccordionDetails>
+                        <Typography>Detalhes</Typography>
+                    </AccordionDetails>
+
+                </AccordionStyled>
+
+            </Container>
+
         </SectionContainer>
 
 
